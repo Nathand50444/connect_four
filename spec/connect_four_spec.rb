ec66571a924ad73325da0704fa0e98ff97a19dec
NathanD <nathand50444@gmail.com>
2024-11-04 21:16:08 +0000

    tests added for turn_count and valid_move? methods

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,1 +1,30 @@
 # instructions -> https://www.theodinproject.com/lessons/ruby-connect-four#project-tdd-connect-four
+
+require_relative '../lib/connect_four'
+
+describe ConnectFour do
+  describe '#valid_move?' do
+    it 'returns true for an empty spot' do
+      game = ConnectFour.new
+      expect(game.valid_move?(0)).to eq(true)
+    end
+
+    it 'returns false for a taken spot' do
+      game = ConnectFour.new
+      game.move("ADD MOVE HERE")
+      expect(game.valid_move?(0)).to eq(false)
+    end
+  end
+
+  describe 'turn_count' do
+    it 'returns the number turns performed' do
+      game = ConnectFour.new
+
+      before do
+        game.instance_variable_set(:@board, [" ", " ", " "] ) #42 spaces?
+      end
+
+      expect(game.turn_count).to eq(0)
+    end
+  end
+end
