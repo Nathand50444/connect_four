bbc6a307c3bcb4917206d4de94f72ff506204471
Nathan <139653121+Nathand50444@users.noreply.github.com>
2024-11-13 17:50:32 +0000

    turn_count and current_player working tests

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -3,18 +3,18 @@
 require_relative '../lib/connect_four'
 
 describe ConnectFour do
-  describe '#valid_move?' do
-    it 'returns true for an empty spot' do
-      game = ConnectFour.new
-      expect(game.valid_move?(0)).to eq(true)
-    end
+  # describe '#valid_move?' do
+  #  it 'returns true for an empty spot' do
+  #    game = ConnectFour.new
+  #    expect(game.valid_move?(0)).to eq(true)
+  #  end
 
    # it 'returns false for a taken spot' do
    #   game = ConnectFour.new
    #   game.move("ADD MOVE HERE")
    #   expect(game.valid_move?(0)).to eq(false)
    # end
-  end
+  # end
 
   describe '#input_to_index' do
     it 'returns the index - 1' do
@@ -25,7 +25,6 @@ describe ConnectFour do
   end
 
   describe '#turn_count' do
-    it 'returns the number turns performed' do
       game = ConnectFour.new
 
       before do
@@ -39,53 +38,54 @@ describe ConnectFour do
           ])
       end
 
-      expect(game.turn_count).to eq(4)
-    end
+      it 'shows the turn count is 4' do
+        expect(game.turn_count).to eq(4)
+      end
   end
 
-  describe '#game_over?' do
-    context 'when four X values are in a row horizontally' do
-      let(:game) { ConnectFour.new }
+  # describe '#game_over?' do
+  # context 'when four X values are in a row horizontally' do
+  #   let(:game) { ConnectFour.new }
 
-      before do
-        game.instance_variable_set(:@board, [ 
-          " ", " ", " ", " ", " ", " ", " ", # Row 1 
-          " ", " ", " ", " ", " ", " ", " ", # Row 2 
-          " ", " ", " ", " ", " ", " ", " ", # Row 3 
-          " ", " ", " ", " ", " ", " ", " ", # Row 4 
-          "O", "O", "O", " ", " ", " ", " ", # Row 5 
-          "X", "X", "X", "X", " ", " ", " "  # Row 6 
-          ])
-      end
+  #   before do
+  #     game.instance_variable_set(:@board, [ 
+  #       " ", " ", " ", " ", " ", " ", " ", # Row 1 
+  #       " ", " ", " ", " ", " ", " ", " ", # Row 2 
+  #       " ", " ", " ", " ", " ", " ", " ", # Row 3 
+  #       " ", " ", " ", " ", " ", " ", " ", # Row 4 
+  #       "O", "O", "O", " ", " ", " ", " ", # Row 5 
+  #       "X", "X", "X", "X", " ", " ", " "  # Row 6 
+  #       ])
+  #   end
 
-      it 'returns true and outputs "Player X wins!"' do
-        expect { game.game_over? }.to output("Player X wins!\n").to_stdout
-        expect(game.game_over?).to eq(true)
-      end
-    end
-  end
+  #   it 'returns true and outputs "Player X wins!"' do
+  #     expect { game.game_over? }.to output("Player X wins!\n").to_stdout
+  #     expect(game.game_over?).to eq(true)
+  #   end
+  # end
+  # end
 
-  describe '#game_over?' do
-    context 'when four X values are in a row vertically' do
-      let(:game) { ConnectFour.new }
+  # describe '#game_over?' do
+  # context 'when four X values are in a row vertically' do
+  #   let(:game) { ConnectFour.new }
 
-      before do
-        game.instance_variable_set(:@board, [ 
-          " ", " ", " ", " ", " ", " ", " ", # Row 1 
-          " ", " ", " ", " ", " ", " ", " ", # Row 2 
-          "X", " ", " ", " ", " ", " ", " ", # Row 3 
-          "X", "O", " ", " ", " ", " ", " ", # Row 4 
-          "X", "O", " ", " ", " ", " ", " ", # Row 5 
-          "X", "O", " ", " ", " ", " ", " "  # Row 6 
-          ])
-      end
+  #   before do
+  #     game.instance_variable_set(:@board, [ 
+  #       " ", " ", " ", " ", " ", " ", " ", # Row 1 
+  #       " ", " ", " ", " ", " ", " ", " ", # Row 2 
+  #       "X", " ", " ", " ", " ", " ", " ", # Row 3 
+  #       "X", "O", " ", " ", " ", " ", " ", # Row 4 
+  #       "X", "O", " ", " ", " ", " ", " ", # Row 5 
+  #       "X", "O", " ", " ", " ", " ", " "  # Row 6 
+  #       ])
+  #   end
 
-      it 'returns true and outputs "Player X wins!"' do
-        expect { game.game_over? }.to output("Player X wins!\n").to_stdout
-        expect(game.game_over?).to eq(true)
-      end
-    end
-  end
+  #   it 'returns true and outputs "Player X wins!"' do
+  #     expect { game.game_over? }.to output("Player X wins!\n").to_stdout
+  #     expect(game.game_over?).to eq(true)
+  #   end
+  # end
+  # end
 
  # describe '#game_over?' do
  #   context 'when four X values are in a row' do
@@ -110,8 +110,8 @@ describe ConnectFour do
  # end
 
   describe '#current_player' do
-    context 'when the turn_count is even' do
-      game = ConnectFour.new
+    context 'when the turn count is odd' do
+      let(:game) { ConnectFour.new }
 
       before do
         game.instance_variable_set(:@board, [ 
@@ -124,8 +124,8 @@ describe ConnectFour do
           ])
       end
 
-      it 'returns true and outputs "Player X wins!"' do
-        expect ( game.current_player ).to output("O")
+      it 'returns player "O" is the current player' do
+        expect(game.current_player).to eq("O")
       end
     end
   end
